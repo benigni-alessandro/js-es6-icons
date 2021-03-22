146486995756d549445517e783d86f31130396ba
alessandro <alebeni02@gmail.com>
2021-03-22 17:41:15 +0100

    manca il bonus

---
+++ main.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+  icons();
+
+
+});
+function icons() {
+
+  const icons =[
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+ ];
+ let card = $('.card');
+ let icon = $('.card i');
+ let container = $('.container');
+ icons.forEach((item) => {
+  let card_copy = card.clone();
+  card_copy.removeClass('none');
+  container.append(card_copy);
+  let color;
+  // y añade contenido
+  card_copy.children('i').addClass(`${item.family} ${item.prefix}${item.name} ${item.type}`);
+  card_copy.children('p').text(`${item.name}`.toUpperCase());
+  if (card_copy.children('i').hasClass('animal')) {
+     card_copy.children('i').addClass('blue');
+  } else if (card_copy.children('i').hasClass('vegetable')) {
+     card_copy.children('i').addClass('yellow');
+  } else {
+     card_copy.children('i').addClass('purple');
+  }
+ });
+
+
+}
